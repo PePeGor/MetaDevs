de62c76b48242ea076eaef71703a02f7024a8fe9
MrSpi4ka <ikazmin@meta-devs.io>
2022-01-25 18:58:21 +0300

    Изменил название класс на корректное, добавил вывод значения "високосного года" и применил форматирование.

--- src/lesson_3/LessonThree.java
+++ src/lesson_3/LessonThree.java
@@ -1,12 +1,10 @@
 package lesson_3;
 
-import com.sun.org.apache.xerces.internal.util.SynchronizedSymbolTable;
+public class LessonThree {
 
-public class Lesson_3 {
-
-     public static void main (String[] args){
+    public static void main(String[] args) {
         int num = 1;
-        switch (num){
+        switch (num) {
             case 1:
                 System.out.println("Число равно 1");
                 break;
@@ -20,45 +18,47 @@ public class Lesson_3 {
             default:
                 System.out.println("Число не равно 1, 8, 9");
         }
-        if(num==1){
+        if (num == 1) {
             System.out.println("Число равно 1");
-        } else if(num==8){
+        } else if (num == 8) {
             num++;
-        }else {
+        } else {
             System.out.println("Число не равно 1, 8, 9");
         }
         comparisonsOperators(1);
         loops(2);
         magic(1);
         //Подсчёт количества високосных годов
-        int year=85;
-        while (year>=0){
-            if(year%4==0){
-                System.out.println("Високосный год");
+        int year = 85;
+        while (year >= 0) {
+            if (year % 4 == 0) {
+                System.out.println("Високосный год " + year);
             }
             year--;
         }
     }
-    public static void comparisonsOperators(int num){
-        if(num != 8 && num != 1 && num != 9 ){
+
+    public static void comparisonsOperators(int num) {
+        if (num != 8 && num != 1 && num != 9) {
             System.out.println("Число не равно 1, 8, 9");
         } else System.out.println("Число равно 1, 8, 9");
-        if(num == 0 || num == 1){
+        if (num == 0 || num == 1) {
             System.out.println("Число равно 1 или 0");
         } else {
             System.out.println("Число не равно ни 1, ни 0");
         }
     }
-    public static void loops(int i){
-         int counter = 1;
-         while(counter<100){
-             counter++;
-             System.out.println(counter);
-         }
+
+    public static void loops(int i) {
+        int counter = 1;
+        while (counter < 100) {
+            counter++;
+            System.out.println(counter);
+        }
     }
-    public static int magic(int x){
-        // return x==10 ? 1:0;
-        if(x==10){
+
+    public static int magic(int x) {
+        if (x == 10) {
             return 1;
         } else {
             return 0;
