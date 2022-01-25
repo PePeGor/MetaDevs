d095e524439e2d73a87b23ed3742ad812175fe24
MrSpi4ka <ikazmin@meta-devs.io>
2022-01-25 18:44:27 +0300

    Изменил название класса на корректное.

--- src/ru/list/real_pcy/LessonTwo.java
+++ src/ru/list/real_pcy/LessonTwo.java
@@ -2,13 +2,13 @@ package ru.list.real_pcy;
 
 import java.util.Scanner;
 
-public class Lesson_2 {
+public class LessonTwo {
     public static void main(String[] args) {
         final int MONTHS = 12;
         int a = 10;
         int b = 8;
         int d = multiply(a, b);
-        sum(a,b);
+        sum(a, b);
         sum(3, a);
         sum(5, 23);
 
@@ -49,8 +49,9 @@ public class Lesson_2 {
         if (x == 0) {
             System.out.println("x равно 0");
         } else
-        System.out.println(z);
+            System.out.println(z);
     }
+
     static int multiply(int x, int y) {
         return x * y;
     }
