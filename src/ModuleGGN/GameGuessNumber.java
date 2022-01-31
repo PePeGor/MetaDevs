13dec674bbbf0f0c567135cf94d84faa1f743ec1
MrSpi4ka <ikazmin@meta-devs.io>
2022-01-31 20:42:29 +0300

    Отформатировал код.

--- src/ModuleGGN/GameGuessNumber.java
+++ src/ModuleGGN/GameGuessNumber.java
@@ -3,7 +3,7 @@ package ModuleGGN;
 import java.util.Scanner;
 
 public class GameGuessNumber {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         // maxRangeOfRandom 0 - 100
         int rangeOfRandom = 100;
         // userAttempts
@@ -11,21 +11,20 @@ public class GameGuessNumber {
 
         System.out.println("Я загадал случайное число от 1 до " + rangeOfRandom);
 
-        for (int i = 1; i<=numbOfAttempts; i++) {
-            System.out.println("Попытка № +" +i);
+        for (int i = 1; i <= numbOfAttempts; i++) {
+            System.out.println("Попытка № +" + i);
             int userInput = new Scanner(System.in).nextInt();
-            if(userInput<mathRandom()){
+            if (userInput < mathRandom()) {
                 System.out.println("Введенное число меньше загаданого");
-            }else if (userInput>mathRandom()){
+            } else if (userInput > mathRandom()) {
                 System.out.println("Введённое число больше загаданного");
-            }else {
+            } else {
                 System.out.println("Вы угадали");
             }
         }
-
     }
 
-    public static int mathRandom(){
+    public static int mathRandom() {
         int rangeOfRandom = 100;
         int mathRnd = (int) (Math.random() * (rangeOfRandom + 1));
         return mathRnd;
