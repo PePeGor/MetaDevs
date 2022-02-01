7d4111a8cd462abe3263437723bddafecaf27733
MrSpi4ka <ikazmin@meta-devs.io>
2022-02-01 18:38:10 +0300

    Исправил программу, теперь число загадывается правильно.

--- src/ModuleGGN/GameGuessNumber.java
+++ src/ModuleGGN/GameGuessNumber.java
@@ -4,19 +4,19 @@ import java.util.Scanner;
 
 public class GameGuessNumber {
     public static void main(String[] args) {
-        // maxRangeOfRandom 0 - 100
+
         int rangeOfRandom = 100;
-        // userAttempts
         int numbOfAttempts = 10;
+        int mathRandomNumber = mathRandom();
 
         System.out.println("Я загадал случайное число от 1 до " + rangeOfRandom);
 
         for (int i = 1; i <= numbOfAttempts; i++) {
             System.out.println("Попытка № +" + i);
             int userInput = new Scanner(System.in).nextInt();
-            if (userInput < mathRandom()) {
+            if (userInput < mathRandomNumber) {
                 System.out.println("Введенное число меньше загаданого");
-            } else if (userInput > mathRandom()) {
+            } else if (userInput > mathRandomNumber) {
                 System.out.println("Введённое число больше загаданного");
             } else {
                 System.out.println("Вы угадали");
