78753d8e6d205cd75f051d02e392789b0502005f
MrSpi4ka <ikazmin@meta-devs.io>
2022-02-15 19:52:42 +0300

    Передал параметр rangeOfRandom в метод. Добавил оператор "break" для прекращения цикла по угадыванию. Немного поменял имена переменных.

--- src/ModuleGGN/GameGuessNumber.java
+++ src/ModuleGGN/GameGuessNumber.java
@@ -6,26 +6,26 @@ public class GameGuessNumber {
     public static void main(String[] args) {
 
         int rangeOfRandom = 100;
-        int numbOfAttempts = 10;
-        int mathRandomNumber = mathRandom();
+        int winOrDie = 10;
+        int divineRandom = mathRandom(rangeOfRandom);
 
         System.out.println("Я загадал случайное число от 1 до " + rangeOfRandom);
 
-        for (int i = 1; i <= numbOfAttempts; i++) {
+        for (int i = 1; i <= winOrDie; i++) {
             System.out.println("Попытка №" + i);
             int userInput = new Scanner(System.in).nextInt();
-            if (userInput < mathRandomNumber) {
+            if (userInput < divineRandom) {
                 System.out.println("Введенное число меньше загаданого");
-            } else if (userInput > mathRandomNumber) {
+            } else if (userInput > divineRandom) {
                 System.out.println("Введённое число больше загаданного");
             } else {
                 System.out.println("Вы угадали");
+                break;
             }
         }
     }
 
-    public static int mathRandom() {
-        int rangeOfRandom = 100;
+    public static int mathRandom(int rangeOfRandom) {
         int mathRnd = (int) (Math.random() * (rangeOfRandom + 1));
         return mathRnd;
     }
