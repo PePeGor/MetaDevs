41d334ceea9c9999a654b478db9c0f609693b456
MrSpi4ka <ikazmin@meta-devs.io>
2022-02-15 19:30:22 +0300

    Добавил строки для разграничения задач.

--- src/lesson_4/LessonFour.java
+++ src/lesson_4/LessonFour.java
@@ -7,12 +7,16 @@ public class LessonFour {
         for (int j = 0; j < 6; j++) {
             System.out.println("Год " + year++);
         }
+
+
         int[] araiV1 = new int[]{1, 2, 3, 4, 5};
         System.out.println(araiV1[4]);
         araiV1[3] = 67;
         for (int j = 0; j < araiV1.length; j++) {
             System.out.println(araiV1[j]);
         }
+
+
         //foreach и работа с массивом
         for (int item : araiV1) {
             System.out.println(item);
